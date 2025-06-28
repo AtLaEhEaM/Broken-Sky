eabdd58293a3eeedf9bc406777030c1367683244
AtLaEhEaM <99670121+AtLaEhEaM@users.noreply.github.com>
2025-06-28 22:52:32 +0500

    more stuff

--- Assets/Scripts/Narration.cs
+++ Assets/Scripts/Narration.cs
@@ -4,12 +4,20 @@ public class NarrationTrigger : MonoBehaviour
 {
     public AudioClip narrationClip;
     public AudioSource audioSource;
+    public bool checkpoint = false;
+    public Vector3 point;
+    public Transform checkpointloc;
 
-    private void OnTriggerEnter(Collider other)
+     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && narrationClip != null && audioSource != null)
         {
             audioSource.PlayOneShot(narrationClip);
+            if (checkpoint)
+            {
+                checkpointloc.localPosition = point;
+            }
+            Destroy(this);
         }
     }
 }
